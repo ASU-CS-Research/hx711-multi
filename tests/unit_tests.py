05d89405f5a985d57b60e01cd7c1aab6f04c340e
gittinhubbed <graberj@appstate.edu>
2021-10-05 13:38:40 -0400

    updated to match PEP 8 standards

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,16 +4,18 @@
 import unittest
 from hx711_multi import HX711
 
+
 class TestStringMethods(unittest.TestCase):
 
     def test_hx711_inputs(self):
         # check to make sure good inputs work
-        good = HX711([1,2,3], 4, 128, 'A', simulate_pi=True)
+        good = HX711([1, 2, 3], 4, 128, 'A')
         self.assertIsInstance(good, HX711)
         # check that it fails with bad dout_pins
-        self.assertRaises(TypeError, HX711, [1,2,False], 4, 128, 'A', simulate_pi=True)
+        self.assertRaises(TypeError, HX711, [1, 2, False], 4, 128, 'A', simulate_pi=True)
         # check that it fails with bad sck_pin
-        self.assertRaises(TypeError, HX711, [1,2,3], False, 128, 'A', simulate_pi=True)
+        self.assertRaises(TypeError, HX711, [1, 2, 3], False, 128, 'A', simulate_pi=True)
+
 
 if __name__ == '__main__':
     unittest.main()
