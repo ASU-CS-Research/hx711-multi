f10f83f020d1e4380643fc4608204283fd72ad48
gittinhubbed <graberj@appstate.edu>
2021-10-05 13:35:21 -0400

    Updated to meet PEP 8 standards

--- src/hx711_multi/hx711.py
+++ src/hx711_multi/hx711.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 This file holds HX711 class and ADC class which is used within HX711 in order to track multiple ADCs
@@ -11,23 +10,24 @@ from .utils import convert_to_list
 from logging import getLogger, Logger, handlers, Formatter
 import os
 
+
 class HX711:
     """
     HX711 class holds data for one or multiple ADCs.
     All ADCs must be using the same clock signal and be the same channel and gain setting
 
     Args:
-        dout_pin (int or [int]): Raspberry Pi GPIO pins where data from HX711 is received
+        dout_pins (int or [int]): Raspberry Pi GPIO pins where data from HX711 is received
         sck_pin (int): Raspberry Pi clock output pin where sck signal to HX711 is sent
-        gain_channel_A (int): Optional, by default value 128
+        channel_a_gain (int): Optional, by default value 128
             Options (128 || 64)
-        select_channel (str): Optional, by default 'A'
+        channel_select (str): Optional, by default 'A'
             Options ('A' || 'B')
         all_or_nothing (bool): Optional, by default True
             if True will only read from scales if all dout pins are ready
             if False, will try for the maximum number of loops for ready-check, and read from the ones that are ready
                 (this will be a slower sampling rate if one or more scales is not ready)
-        log_level (str or int): Optional, prints out info to consolde based on level of log
+        log_level (str or int): Optional, prints out info to console based on level of log
             Options (0:'NOTSET', 10:'DEBUG', 20:'INFO', 30:'WARN', 40:'ERROR', 50:'CRITICAL')
 
     Raises:
@@ -39,7 +39,7 @@ class HX711:
     def __init__(self,
                  dout_pins,
                  sck_pin: int,
-                 channel_A_gain: int = 128,
+                 channel_a_gain: int = 128,
                  channel_select: str = 'A',
                  all_or_nothing: bool = True,
                  log_level: str = 'WARN',
@@ -60,7 +60,7 @@ class HX711:
         self._sck_pin = sck_pin
         # init GPIO before channel because a read operation is required for channel initialization
         self._init_gpio()
-        self._channel_A_gain = channel_A_gain
+        self._channel_a_gain = channel_a_gain
         self._channel_select = channel_select
         self._init_adcs()
         # perform a read which sets channel and gain
@@ -95,17 +95,17 @@ class HX711:
         self.__sck_pin = sck_pin
 
     @property
-    def _channel_A_gain(self):
-        return self.__channel_A_gain
+    def _channel_a_gain(self):
+        return self.__channel_a_gain
 
-    @_channel_A_gain.setter
-    def _channel_A_gain(self, channel_A_gain):
+    @_channel_a_gain.setter
+    def _channel_a_gain(self, channel_a_gain):
         # check channel_select for type and value. Default is A if None
-        if channel_A_gain not in [128, 64]:
+        if channel_a_gain not in [128, 64]:
             # raise error if channel not 128 or 64
             raise TypeError(
-                f'channel_A_gain must be A or B.\nReceived channel_A_gain: {channel_A_gain}')
-        self.__channel_A_gain = channel_A_gain
+                f'channel_a_gain must be A or B.\nReceived channel_a_gain: {channel_a_gain}')
+        self.__channel_a_gain = channel_a_gain
 
     @property
     def _channel_select(self):
@@ -144,19 +144,20 @@ class HX711:
 
         # check if ready a maximum of 20 times (~200ms)
         # should usually be about 10 iterations with 10Hz sampling
+        i = 0
         for i in range(20):
             # confirm all dout pins are ready (LOW)
-            if all([adc._is_ready() for adc in self._adcs]):
+            if all([adc.is_ready() for adc in self._adcs]):
                 break
             else:
                 # if not ready sleep for 10ms before next iteration
                 sleep(0.01)
-        ready = all([adc._ready for adc in self._adcs])
+        ready = all([adc.ready for adc in self._adcs])
         if ready:
             self._logger.debug(
                 f'checked sensor readiness, completed after {i+1} iterations')
         else:
-            self._logger.warn(
+            self._logger.warning(
                 f'checked sensor readiness, not ready after {i+1} iterations')
         return ready
 
@@ -176,7 +177,7 @@ class HX711:
         # check if the hx 711 did not turn off...
         if pulse_end - pulse_start >= 0.00006:
             # if pd_sck pin is HIGH for 60 us and more than the HX 711 enters power down mode.
-            self._logger.warn(
+            self._logger.warning(
                 f'sck pulse lasted for longer than 60us\nTime elapsed: {pulse_end - pulse_start}')
             return False
         return True
@@ -191,14 +192,14 @@ class HX711:
         B, 32 : total pulses = 26 (24 read data, 2 extra to set dout back to high)
 
         Returns:
-            bool: True if pulsees were all successful
+            bool: True if pulses were all successful
         """
 
         # get number of pulses based on channel configuration
         num_pulses = 2  # default 2 for channel B
-        if self._channel_select == 'A' and self._channel_A_gain == 128:
+        if self._channel_select == 'A' and self._channel_a_gain == 128:
             num_pulses = 1
-        elif self._channel_select == 'A' and self._channel_A_gain == 64:
+        elif self._channel_select == 'A' and self._channel_a_gain == 64:
             num_pulses = 3
 
         # pulse num_pulses
@@ -214,7 +215,8 @@ class HX711:
         operation:
             1) set SCK output HIGH, loop until all dout pins are LOW (_prepare_to_read)
             2) read first 24 bits of each ADC by pulsing SCK output for each bit
-            3) set channel gain following read by pulsing SCK to result in a total of 25, 26, or 27 SCK pulses for a read operation (see documentation)
+            3) set channel gain following read by pulsing SCK to result in a total of 25, 26, or 27 SCK pulses
+            for a read operation (see documentation)
 
         Returns:
             bool : returns True if successful. Readings are assigned to ADC objects
@@ -223,8 +225,7 @@ class HX711:
         adc: ADC
         # init each ADC raw read data
         for adc in self._adcs:
-            adc._init_raw_read()
-
+            adc.init_raw_read()
         # prepare for read by setting SCK pin and checking that each ADC is ready
         # if _all_or_nothing and not _prepare_to_read, then do not perform the read
         if not self._prepare_to_read() and self._all_or_nothing:
@@ -236,12 +237,12 @@ class HX711:
             if not self._pulse_sck_high():
                 return False
             for adc in self._adcs:
-                if adc._ready:
-                    adc._shift_and_read()
+                if adc.ready:
+                    adc.shift_and_read()
         # finalize each ADC raw read
         for adc in self._adcs:
-            if adc._ready:
-                adc._finish_raw_read()
+            if adc.ready:
+                adc.finish_raw_read()
 
         # set channel after read
         if not self._write_channel_gain():
@@ -261,20 +262,21 @@ class HX711:
 
         if not (1 <= readings_to_average <= 10000):
             raise ValueError(
-                f'Parameter "readings_to_average" input to read_raw() is way too high... Received: {readings_to_average}')
+                f'Parameter "readings_to_average" input to read_raw() is way too high... '
+                f'Received: {readings_to_average}')
 
         adc: ADC
         # init each adc for a set of reads
         for adc in self._adcs:
-            adc._init_set_of_reads()
+            adc.init_set_of_reads()
         # perform reads
         for _ in range(readings_to_average):
             self._read()
 
         # for each adc, calculate measurement values
         for adc in self._adcs:
-            if adc._ready:
-                adc._calculate_measurement()
+            if adc.ready:
+                adc.calculate_measurement()
 
         all_adc_vars = "\n".join([str(vars(adc)) for adc in self._adcs])
         self._logger.info(
@@ -307,7 +309,9 @@ class HX711:
 
         if not (1 <= readings_to_average <= 10000):
             raise ValueError(
-                f'Parameter "readings_to_average" input to read_raw() is way too high... Received: {readings_to_average}')
+                f'Parameter "readings_to_average" input to read_raw() is way too high... '
+                f'Received: {readings_to_average}'
+            )
 
         if not use_prev_read:
             # perform raw read operation to get means and then offset and divide by weight multiple
@@ -368,19 +372,23 @@ class HX711:
             raise Exception(f'Failed to zero ADCs. Readings: {str(readings)}')
         adc: ADC
         for adc in self._adcs:
-            if adc._ready:
+            if adc.ready:
                 self._logger.debug(
-                    f'zeroing with {len(adc._reads_filtered)} datapoints')
+                    f'zeroing with {len(adc.reads_filtered)} data points')
                 adc.zero_from_last_measurement()
 
     def set_weight_multiples(self, weight_multiples, adc_indices=None, dout_pins=None):
         """
-        Sets the weight mutliples for ADCs to be used when calculating weight
-        Example: scale indicates value of 5000 for 1 gram on scale, weight_multiple = 5000 (to convert to weight in grams)
+        Sets the weight multiples for ADCs to be used when calculating weight
+        Example: scale indicates value of 5000 for 1 gram on scale,
+        weight_multiple = 5000 (to convert to weight in grams)
 
         Args:
             weight_multiples (float | list of floats): value(s) to be set on ADCs
-            adc_indices (int | list of ints, optional): indices of ADC with reference to ordering of dout pins during initialization
+
+            adc_indices (int | list of ints, optional): indices of ADC with reference
+            to ordering of dout pins during initialization
+
             dout_pins (float | list of floats, optional): use this instead of adc_indices if desired
 
         """
@@ -394,7 +402,7 @@ class HX711:
         adc: ADC
         if dout_pins is not None:
             dout_pins = convert_to_list(dout_pins, _type=int)
-            adcs = [adc for adc in self._adcs if adc._dout_pin in dout_pins]
+            adcs = [adc for adc in self._adcs if adc.dout_pin in dout_pins]
         else:
             adc_indices = convert_to_list(adc_indices, _type=int)
             adcs = [adc for (i, adc) in enumerate(
@@ -418,13 +426,13 @@ class HX711:
 
     def get_offset(self):
         """
-        Gets zero_offset of the all adc's:
+        Gets zero_offset of the all adcs:
 
         Returns: (list) A list of all of the offsets of adcs.
         TODO: Extend functionality to include returning more than one offset. (Currently is only tested on one adc)
         """
         adc: ADC
-        offsets = [adc.get_zero_offset() for adc in self._adcs]
+        offsets = [adc.zero_offset for adc in self._adcs]
         return offsets
 
 
@@ -445,16 +453,18 @@ class ADC:
         _current_raw_read (int):    current raw read from binary bit read
         raw_reads ([int]):          raw reads from binary bit read as 2s complement from ADC
         reads ([signed int])        raw reads after convert to signed integer
-        _max_stdev (int):           max standard deviation value of raw reads (future todo: expose for user input? Does this vary per hardware?)
-        _reads_filtered ([int])     filtered reads after removing failed reads and bad datapoints
-        _max_number_of_stdev_from_med (float): maximium number of deviations from median (future todo: expose for user input?)
+        _max_stdev (int):           max standard deviation value of raw reads
+        (future todo: expose for user input? Does this vary per hardware?)
+        _reads_filtered ([int])     filtered reads after removing failed reads and bad data points
+        _max_number_of_stdev_from_med (float): maximum number of deviations from median
+        (future todo: expose for user input?)
         _read_med (float)           median of reads
         _devs_from_med ([float]):   deviations of reads from median
         _read_stdev (float)         st dev of reads
         _ratios_to_stdev ([float]): ratios of dev from med of each read to the st dev of all the data
-        measurement (float):        mean value of raw reads after filtering
-        measurement_from_zero (float): measurement minus offset
-        weight (float):             measurement_from_zero divided by weight_multiple
+        _measurement (float):        mean value of raw reads after filtering
+        _measurement_from_zero (float): measurement minus offset
+        _weight (float):             measurement_from_zero divided by weight_multiple
     """
 
     def __init__(self,
@@ -476,17 +486,18 @@ class ADC:
         self._devs_from_med = []
         self._read_stdev = 0.
         self._ratios_to_stdev = []
-        self.measurement = None
-        self.measurement_from_zero = None
-        self.weight = None
+        self._measurement = None
+        self._measurement_from_zero = None
+        self._weight = None
+        self._current_signed_value = None
 
     def zero_from_last_measurement(self):
         """ sets offset based on current value for measurement """
-        if self.measurement:
-            self.zero(self.measurement)
+        if self._measurement:
+            self.zero(self._measurement)
         else:
             raise ValueError(f'Trying to zero ADC (dout={self._dout_pin}) with a bad mean value. '
-                             f'Value of measurement: {self.measurement}')
+                             f'Value of measurement: {self._measurement}')
 
     def zero(self, offset: float = None):
         """ sets offset based on current value for measurement """
@@ -495,17 +506,23 @@ class ADC:
         else:
             raise ValueError(f'No offset provided to zero() function')
 
-    def get_zero_offset(self):
+    @property
+    def ready(self):
+        return self._ready
+
+    @property
+    def zero_offset(self):
         return self._zero_offset
 
-    def get_weight_multiple(self):
+    @property
+    def weight_multiple(self):
         return self._weight_multiple
 
     def set_weight_multiple(self, weight_multiple: float):
         """ simply sets multiple. example: scale indicates value of 5000 for 1 gram on scale, weight_multiple = 5000 """
         self._weight_multiple = weight_multiple
 
-    def _init_set_of_reads(self):
+    def init_set_of_reads(self):
         """ init arrays and calculated values before beginning a set of reads for a measurement """
         self.raw_reads = []
         self.reads = []
@@ -514,17 +531,17 @@ class ADC:
         self._devs_from_med = []
         self._read_stdev = 0.
         self._ratios_to_stdev = []
-        self.measurement = None
-        self.measurement_from_zero = None
+        self._measurement = None
+        self._measurement_from_zero = None
         # self.weight = None ## don't overwrite weight so we can keep using older weight value
-        self._init_raw_read()
+        self.init_raw_read()
 
-    def _init_raw_read(self):
+    def init_raw_read(self):
         """ set raw read value to zero, so each bit can be shifted into this value """
         self._ready = False
         self._current_raw_read = 0
 
-    def _is_ready(self):
+    def is_ready(self):
         """ return True if already _ready or GPIO input is zero """
         if self._ready:
             return True
@@ -532,12 +549,12 @@ class ADC:
             self._ready = (GPIO.input(self._dout_pin) == 0)
             return self._ready
 
-    def _shift_and_read(self):
+    def shift_and_read(self):
         """ left shift by one bit then bitwise OR with the new bit """
         self._current_raw_read = (
             self._current_raw_read << 1) | GPIO.input(self._dout_pin)
 
-    def _finish_raw_read(self):
+    def finish_raw_read(self):
         """ append current raw read value and signed value to raw_reads list and reads list """
         self.raw_reads.append(self._current_raw_read)
         # convert to signed value
@@ -557,14 +574,14 @@ class ADC:
             return None  # return None because the data is invalid
         # calculate int from 2's complement
         # check if the sign bit is 1, indicating a negative number
-        if (raw_value & 0x800000):
+        if raw_value & 0x800000:
             # convert from 2's complement to negative int
             signed_value = -((raw_value ^ 0xffffff) + 1)
         else:  # else do not do anything the value is positive number
             signed_value = raw_value
         return signed_value
 
-    def _calculate_measurement(self):
+    def calculate_measurement(self):
         """
         analyzes read values to calculate mean value
             1) filter by valid data only
@@ -583,7 +600,7 @@ class ADC:
             # no values after filter, so return False to indicate no read value
             return False
         elif len(self._reads_filtered) == 1:
-            self.measurement = self._reads_filtered[0]
+            self._measurement = self._reads_filtered[0]
             return True
 
         # get median and deviations from med
@@ -595,16 +612,18 @@ class ADC:
         # filter by number of standard deviations from med
         if self._read_stdev > self._max_stdev:
             # if standard deviation is too large, the scale isn't actually ready
-            # sometimes with a bad scale connection, the bit will come back ready out of chance and the binary values are garbage data
+            # sometimes with a bad scale connection, the bit will come back ready out of chance
+            # and the binary values are garbage data
             self._ready = False
-            self._logger.warn(
-                f'ADC (dout {self._dout_pin}) not ready, stdev from median was over {self._max_stdev}: {self._read_stdev}')
+            self._logger.warning(
+                f'ADC (dout {self._dout_pin}) not ready, stdev from '
+                f'median was over {self._max_stdev}: {self._read_stdev}')
         elif self._read_stdev:
             self._ratios_to_stdev = [(dev / self._read_stdev)
                                      for dev in self._devs_from_med]
         else:
             # stdev is 0. Therefore set to the median
-            self.measurement = self._read_med
+            self._measurement = self._read_med
             return True
         _new_reads_filtered = []
         for (read_val, ratio) in zip(self._reads_filtered, self._ratios_to_stdev):
@@ -616,8 +635,24 @@ class ADC:
         if not self._reads_filtered:
             # no values after filter, so return False to indicate no read value
             return False
-        self.measurement = mean(self._reads_filtered)
-        self.measurement_from_zero = self.measurement - self._zero_offset
-        self.weight = self.measurement_from_zero / self._weight_multiple
+        self._measurement = mean(self._reads_filtered)
+        self._measurement_from_zero = self._measurement - self._zero_offset
+        self._weight = self._measurement_from_zero / self._weight_multiple
 
         return True
+
+    @property
+    def dout_pin(self):
+        return self._dout_pin
+
+    @property
+    def reads_filtered(self):
+        return self._reads_filtered
+
+    @property
+    def measurement_from_zero(self):
+        return self._measurement_from_zero
+
+    @property
+    def weight(self):
+        return self._weight
